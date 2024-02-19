31d667a182872b7a63a7d36ea8a81170db3e2079
MrHurried <Briers.matisse@gmail.com>
2024-02-19 10:26:51 +0100

    Made it retro

--- Assets/EnemySpike_Bounce.cs
+++ Assets/EnemySpike_Bounce.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class EnemySpike_Bounce : MonoBehaviour
 {
+    [SerializeField] float movementSpeed = 5f;
+
     // Start is called before the first frame update
     void Start()
     {
