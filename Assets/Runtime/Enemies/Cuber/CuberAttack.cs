1c0510c32fe9795c19aaff369888acf8165cf39e
MrHurried <Briers.matisse@gmail.com>
2024-02-20 18:37:44 +0100

    Finished the cuber (thank the lord)

--- Assets/Runtime/Enemies/Cuber/CuberAttack.cs
+++ Assets/Runtime/Enemies/Cuber/CuberAttack.cs
@@ -9,6 +9,7 @@ using UnityEngine.UIElements;
 public class CuberAttack : MonoBehaviour
 {
     [SerializeField] private bool isMoving;
+    [SerializeField] private bool shouldMoveTowardsB = true;
     private Vector3 posB;
     private Vector3 posA;
 
@@ -17,7 +18,7 @@ public class CuberAttack : MonoBehaviour
     [SerializeField] AnimationCurve curve;
 
     [SerializeField] float timer = 0f;
-    [SerializeField] float durationToAndBack;
+    [SerializeField] float speed;
 
     private Vector3 rayUpOrigin;
     private Vector3 rayDownOrigin;
@@ -25,6 +26,13 @@ public class CuberAttack : MonoBehaviour
     private Vector3 rayRightOrigin;
 
     private Renderer rend;
+
+    private float distanceToTravelUpwards;
+    private float distanceToTravelDownwards;
+    private float distanceToTravelRightwards;
+    private float distanceToTravelLeftwards;
+
+    private float distanceToTravel;
     // Start is called before the first frame update
     void Start()
     {
@@ -32,12 +40,17 @@ public class CuberAttack : MonoBehaviour
 
         posA = transform.position;
 
+        rayUpOrigin = transform.position + new Vector3(0f, rend.bounds.size.y / 1.8f, 0f);
+        rayDownOrigin = transform.position - new Vector3(0f, rend.bounds.size.y / 1.8f, 0f);
+        rayRightOrigin = transform.position + new Vector3(rend.bounds.size.x / 1.8f, 0f, 0f);
+        rayLeftOrigin = transform.position - new Vector3(rend.bounds.size.x / 1.8f, 0f, 0f);
+
         AssignBPositions();
 
-        rayUpOrigin = transform.position + new Vector3(0f, 3f, 0f);
-        rayDownOrigin = transform.position - new Vector3(0f, 3f, 0f);
-        rayRightOrigin = transform.position + new Vector3(3f, 0f, 0f);    
-        rayLeftOrigin = transform.position - new Vector3(3f, 0f, 0f);
+        distanceToTravelUpwards = Vector3.Distance(bPositions[0], posA);
+        distanceToTravelDownwards = Vector3.Distance(bPositions[1], posA);
+        distanceToTravelLeftwards = Vector3.Distance(bPositions[2], posA);
+        distanceToTravelRightwards = Vector3.Distance(bPositions[3], posA);
     }
 
     void DrawDebugRays()
@@ -78,24 +91,22 @@ public class CuberAttack : MonoBehaviour
         Physics.Raycast(ray, out RaycastHit hitInfoRight, 9999f);
         Debug.DrawRay(ray.origin, ray.direction * 9999f);
 
-        Vector3 upBPos = hitInfoUp.point;//- new Vector3(0f, rend.bounds.size.y / 2f, 0f);
-        Vector3 downBPos = hitInfoDown.point;//+ new Vector3(0f, rend.bounds.size.y / 2f, 0f);
-        Vector3 leftBPos = hitInfoLeft.point;//+ new Vector3(rend.bounds.size.x / 2f, 0f, 0f);
-        Vector3 rightBPos = hitInfoRight.point;//- new Vector3(rend.bounds.size.x / 2f, 0f, 0f);
+        Vector3 upBPos = hitInfoUp.point - new Vector3(0f, rend.bounds.size.y / 1.8f, 0f);
+        Vector3 downBPos = hitInfoDown.point + new Vector3(0f, rend.bounds.size.y / 1.8f, 0f);
+        Vector3 leftBPos = hitInfoLeft.point + new Vector3(rend.bounds.size.x / 1.8f, 0f, 0f);
+        Vector3 rightBPos = hitInfoRight.point - new Vector3(rend.bounds.size.x / 1.8f, 0f, 0f);
 
-        if (upBPos != null)
-            bPositions[0] = upBPos;
-        if (downBPos != null)
-            bPositions[1] = downBPos;
-        if (leftBPos != null)
-            bPositions[2] = leftBPos;
-        if (rightBPos != null)
-            bPositions[3] = rightBPos;
+        bPositions[0] = upBPos;
+        bPositions[1] = downBPos;
+        bPositions[2] = leftBPos;
+        bPositions[3] = rightBPos;
     }
 
     // Update is called once per frame
     void Update()
     {
+
+        DrawDebugRays();
 
         if (!isMoving)
             CheckIfPlayerIsInVision();
@@ -131,24 +142,28 @@ public class CuberAttack : MonoBehaviour
 
         if (hitInfoUp.collider != null && hitInfoUp.collider.gameObject.CompareTag("Player"))
         {
+            distanceToTravel = distanceToTravelUpwards;
             posB = bPositions[0];
             isMoving = true;
             Debug.Log("cuber started moving up");
         }
         if (hitInfoDown.collider != null && hitInfoDown.collider.gameObject.CompareTag("Player"))
         {
+            distanceToTravel = distanceToTravelDownwards;
             posB = bPositions[1];
             isMoving = true;
             Debug.Log("cuber started moving down");
         }
         if (hitInfoLeft.collider != null && hitInfoLeft.collider.gameObject.CompareTag("Player"))
         {
+            distanceToTravel = distanceToTravelLeftwards;
             posB = bPositions[2];
             isMoving = true;
             Debug.Log("cuber started moving left");
         }
         if (hitInfoRight.collider != null && hitInfoRight.collider.gameObject.CompareTag("Player"))
         {
+            distanceToTravel = distanceToTravelRightwards;
             posB = bPositions[3];
             isMoving = true;
             Debug.Log("cuber started moving right");
@@ -159,13 +174,33 @@ public class CuberAttack : MonoBehaviour
     private void Move()
     {
         timer += Time.deltaTime;
-        transform.position = Vector3.Lerp(posA, posB, curve.Evaluate(timer / durationToAndBack));
-
-        if (timer >= durationToAndBack)
+        if (shouldMoveTowardsB)
         {
-            timer = 0f;
-            transform.position = posA;
-            isMoving = false;
+            transform.position = Vector3.MoveTowards(transform.position, posB, curve.Evaluate(timer) * speed * Time.deltaTime);
+
+            float distanceBetweenCubeAndPointB = Vector3.Distance(transform.position, posB);
+
+            if (distanceBetweenCubeAndPointB < .01f)
+            {
+                transform.position = posB;
+                shouldMoveTowardsB = false;
+                timer = 0f;
+            }
         }
+        else
+        {
+            transform.position = Vector3.MoveTowards(transform.position, posA, curve.Evaluate(timer) * speed * Time.deltaTime);
+
+            float distanceBetweenCubeAndPointA = Vector3.Distance(transform.position, posA);
+
+            if (distanceBetweenCubeAndPointA < .01f)
+            {
+                transform.position = posA;
+                isMoving = false;
+                shouldMoveTowardsB = true;
+                timer = 0f;
+            }
+        }
+        
     }
 }
